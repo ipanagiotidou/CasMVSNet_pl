62d0d2300d9817aa856f773629acac81a4789e0c
Ioanna Panagiotidou <92786335+ipanagiotidou@users.noreply.github.com>
2023-05-15 18:45:47 +0200

    Update train.py

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ class MVSSystem(LightningModule):
         log = {'lr': get_learning_rate(self.optimizer)}
         imgs, proj_mats, depths, masks, init_depth_min, depth_interval = \
             self.decode_batch(batch)
-        results = self(imgs, proj_mats, init_depth_min, depth_interval)
+        results = self(imgs, proj_mats, init_depth_min, depth_interval)   # I: from mvsnet.py the results are:  results[f"depth_{l}"] = depth_l, and results[f"confidence_{l}"] = confidence_l 
         log['train/loss'] = loss = self.loss(results, depths, masks)
         
         with torch.no_grad():
