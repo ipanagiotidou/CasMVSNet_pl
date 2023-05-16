b9002b9af33e9d7b63bc1849651bc16562e6ceb3
Ioanna Panagiotidou <92786335+ipanagiotidou@users.noreply.github.com>
2023-05-16 15:22:24 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -62,8 +62,8 @@ class CustomLoss():  # nn.Module
             # multiply the loss term with the planar_mask to filter out pixels that do not belong to planar regions, therefore there is no need to contribute to the Loss. 
             # NOTE: the Laplacian operator can produce both positive and negative values in the response image, thus the absolute values are taken.
             # --> Question: what is the response of the Laplacian operator and how can I use it to switch on or off the loss. --> DDL-MVS uses BETA = -20 which turns exp(-20 * 0) = exp(0) = 1 and exp(-20 * 1) = 0.
-            tv_h = (planar_mask_l[:,:,1:-1,:] * mask_l[:,:,1:-1,:] * torch.abs(laplacian_depthy) * torch.exp(- torch.abs(laplacian_semanticy[:,:,1:-1,:]))).sum() # mask_l indicates the valid pixels, planar_mask_l indicates the planar pixels 
-            tv_w = (planar_mask_l[:,:,:,1:-1] * mask_l[:,:,:,1:-1] * torch.abs(laplacian_depthx) * torch.exp(- torch.abs(laplacian_semanticx[:,:,:,1:-1]))).sum() 
+            tv_h = (planar_mask_l[:,:,1:-1,:] * mask_l[:,:,1:-1,:] * torch.abs(laplacian_depthy) * torch.exp(-torch.abs(laplacian_semanticy[:,:,1:-1,:]))).sum() # mask_l indicates the valid pixels, planar_mask_l indicates the planar pixels 
+            tv_w = (planar_mask_l[:,:,:,1:-1] * mask_l[:,:,:,1:-1] * torch.abs(laplacian_depthx) * torch.exp(-torch.abs(laplacian_semanticx[:,:,:,1:-1]))).sum() 
             
             TV2LOSS = 2.5*(tv_h + tv_w)/len(depth1) # 2500
             
