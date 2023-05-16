cc071b24410c257e59e1398c3dcf1453d00b3cdd
Ioanna Panagiotidou <92786335+ipanagiotidou@users.noreply.github.com>
2023-05-16 14:38:52 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -28,26 +28,24 @@ class CustomLoss():  # nn.Module
     # I: --> Lplanar = | second order derivative of depth map | * exp(-| ????? order derivative of semantic map |)
     """ 
     
-    def __init__(self, levels=3,
-                 depth_pred,
-                 depth_gt,
-                 mask,
-                 semantic_map,  
-                 planar_mask,
-                 ): # example of type 'depth_gt: Dict[str, torch.Tensor]'
-        
-        # super(SL1Loss, self).__init__()
+    def __init__(self, levels=3):  # super(SL1Loss, self).__init__()
         self.levels = levels
         self.loss = nn.SmoothL1Loss(reduction='mean') 
-    
-    
-    def forward(self, inputs, targets, masks):         
         
+    
+      
+    
+    
+    def forward(self, inputs, targets, masks, semantic_maps, planar_masks):    
+                        
         loss = 0
         for l in range(self.levels):
             depth_pred_l = inputs[f'depth_{l}']
             depth_gt_l = targets[f'level_{l}']
             mask_l = masks[f'level_{l}']
+            semantic_l = semantic_maps
+            planar_mask = planar_masks
+            
             # TO DO: add the semantic map 
             
             # depth-groundtruth loss --> CasMVSNet (DDL-MVS combines ground-truth and smoothness term in one loss function)
@@ -58,8 +56,8 @@ class CustomLoss():  # nn.Module
             # --> dimensions of the predicted depth is (B, h, w) --> index and slice the height and width dimensions.
             # --> TO DO: resize on the fly the planar mask to match the size of the output depth map of levels 1, 2.             
             # DDL-MVS 
-            laplacian_depthy = torch.abs(2*depth_pred_l[:,1:-1,:] - depth_pred_l[:,:-2,:] - depth_pred_l[:,2:,:])   # [:,1:-1,:] cuts the first and last row 
-            laplacian_depthx = torch.abs(2*depth_pred_l[:,:,1:-1] - depth_pred_l[:,:,:-2] - depth_pred_l[:,:,2:])   # [:,:,1:-1] cuts the first and last col
+            laplacian_depthy = torch.abs(2*depth_pred_l[:,1:-1,:] - depth_pred_l[:,:-2,:] - depth_pred_l[:,2:,:])   # [:,1:-1,:] discard the first and last row 
+            laplacian_depthx = torch.abs(2*depth_pred_l[:,:,1:-1] - depth_pred_l[:,:,:-2] - depth_pred_l[:,:,2:])   # [:,:,1:-1] discard the first and last col
             
             # --> TO DO: apply the Laplacian operator to the semantic map  
             # NOTE: the semantic map can be turned to a binary indicating planar or non-planar areas. This will act as a mask to filter out pixels that we don't want them to contribute to the Loss. (P = 1 - S)
@@ -68,8 +66,8 @@ class CustomLoss():  # nn.Module
             
             # Applying the Laplacian to the semantic map you guarantee that only non-boundary regions contribute to the Loss.
             BETA = -20.  # turns exp(-20 * 0) = exp(0) = 1 and exp(-20 * 1) = 0 
-            tv_h = (mask_l[:,:,1:-1,:]*torch.abs(laplacian_depthy)*torch.exp(BETA*edges_est[f'stage_{l}'][:,:,1:-1,:])).sum()
-            tv_w = (mask_l[:,:,:,1:-1]*torch.abs(laplacian_depthx)*torch.exp(BETA*edges_est[f'stage_{l}'][:,:,:,1:-1])).sum()
+            tv_h = (laplace_depth_mask_l[:,:,1:-1,:]*torch.abs(laplacian_depthy)*torch.exp(BETA*edges_est[f'stage_{l}'][:,:,1:-1,:])).sum()
+            tv_w = (laplace_depth_mask_l[:,:,:,1:-1]*torch.abs(laplacian_depthx)*torch.exp(BETA*edges_est[f'stage_{l}'][:,:,:,1:-1])).sum()
 
             # multiply the loss term with the planar_mask to filter out pixels with no need to contribute to the Loss. 
             TV2LOSS = 2.5*(tv_h + tv_w)/len(depth1) # 2500
