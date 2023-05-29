30eb518173b80278c7aba6f885661cb9feecf099
Ioanna Panagiotidou <92786335+ipanagiotidou@users.noreply.github.com>
2023-05-29 18:50:37 +0200

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,7 +1,9 @@
 from .dtu import DTUDataset
 from .tanks import TanksDataset
 from .blendedmvs import BlendedMVSDataset
+from .eth3d import ETH3Dataset
 
 dataset_dict = {'dtu': DTUDataset,
                 'tanks': TanksDataset,
-                'blendedmvs': BlendedMVSDataset}
+                'blendedmvs': BlendedMVSDataset, 
+                'eth3d': ETH3Dataset}
